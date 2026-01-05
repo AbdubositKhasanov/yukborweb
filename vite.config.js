2186478f9d38857c0dbde09056761d8d81022456
AbdubositKhasanov <xasanovabdubosit@gmail.com>
2026-01-06 02:40:45 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://167.172.68.133:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
       }
     }
+  },
+  // SPA fallback uchun
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    historyApiFallback: true, // Bu localhost uchun
   }
 })
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: process.env.VITE_API_URL || 'http://167.172.68.133:8080',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     }
+//   }
+// })
